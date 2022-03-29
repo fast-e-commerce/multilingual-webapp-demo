5a338e21f8a9d3ce991c786f7ee3486f58a7cf3f
nelson frank <nelsonfr78@outlook.com>
2022-03-29 20:19:02 +0300

    feat: Add simple landing page with chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
+import customTheme from "../utils/theme";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
